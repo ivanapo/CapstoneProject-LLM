8246f6be471f3f8ca7be152df1e7964e282964f8
ivanapo <ivan.2002.mk@gmail.com>
2024-09-15 21:54:40 +0200

    requiremnets update

--- rag_app/get_embedding_function.py
+++ rag_app/get_embedding_function.py
@@ -5,5 +5,5 @@ def get_embedding_function():
     # embeddings = BedrockEmbeddings(
     #     credentials_profile_name="default", region_name="us-east-1"
     # )
-    embeddings = OllamaEmbeddings(model="Mistral")
+    embeddings = OllamaEmbeddings(model="LLama3.1")
     return embeddings
